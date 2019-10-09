2cec9914a57f8f199372834076d662ffae8aceb6
Aaron Greenspan <afg419@gmail.com>
2019-10-09 17:24:04 -0600

    try5

--- src/app/wifi/wifi.page.ts
+++ src/app/wifi/wifi.page.ts
@@ -8,14 +8,14 @@ declare var WifiWizard2: any;
   styleUrls: ['./wifi.page.scss'],
 })
 export class WifiPage implements OnInit {
+  private readonly networks: any
 
   constructor() { }
 
   async ngOnInit() {
     try {
-
-      const x1 = await WifiWizard2.listNetworks()
-      console.log(`listNetworks ${x1}`)
+      this.networks = await WifiWizard2.listNetworks()
+      console.log(`listNetworks ${this.networks}`)
       const x2 = await WifiWizard2.getConnectedSSID()
       console.log(`getConnectedSSID ${x2}`)
       const x3 = await WifiWizard2.getConnectedNetworkID()
@@ -23,7 +23,7 @@ export class WifiPage implements OnInit {
       const x4 = await WifiWizard2.requestPermission()
       console.log(`requestPermission ${x4}`)
       const x5 = await WifiWizard2.scan()
-      console.log(`scan ${x5}`)
+      console.log(`scan ${JSON.stringify(x5)}`)
       const x6 = await WifiWizard2.isWifiEnabled()
       console.log(`isWifiEnabled ${x6}`)
 
