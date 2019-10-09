61f78a4d43c38de69177f1689834164321bbbb70
Aaron Greenspan <afg419@gmail.com>
2019-10-09 17:15:09 -0600

    try4

--- src/app/wifi/wifi.page.ts
+++ src/app/wifi/wifi.page.ts
@@ -13,16 +13,20 @@ export class WifiPage implements OnInit {
 
   async ngOnInit() {
     try {
-      WifiWizard2.listNetworks().then(console.log).catch(console.error)
-      WifiWizard2.getConnectedSSID().then(console.log).catch(console.error)
-      WifiWizard2.getConnectedNetworkID().then(console.log).catch(console.error)
 
-      WifiWizard2.requestPermission().then(p => {
-        console.log(p)
-        WifiWizard2.scan().then(console.log).catch(console.error)
-      })
+      const x1 = await WifiWizard2.listNetworks()
+      console.log(`listNetworks ${x1}`)
+      const x2 = await WifiWizard2.getConnectedSSID()
+      console.log(`getConnectedSSID ${x2}`)
+      const x3 = await WifiWizard2.getConnectedNetworkID()
+      console.log(`getConnectedNetworkID ${x3}`)
+      const x4 = await WifiWizard2.requestPermission()
+      console.log(`requestPermission ${x4}`)
+      const x5 = await WifiWizard2.scan()
+      console.log(`scan ${x5}`)
+      const x6 = await WifiWizard2.isWifiEnabled()
+      console.log(`isWifiEnabled ${x6}`)
 
-      WifiWizard2.isWifiEnabled().then(console.log).catch(console.error)
     } catch (e) {
       console.error(e)
     }
