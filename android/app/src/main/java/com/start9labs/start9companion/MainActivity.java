d94d93a9155fc973ed721c42263f0e0d6e870c4b
Matt Hill <matthewonthemoon@gmail.com>
2020-04-14 18:22:04 -0600

    update android stuff

--- android/app/src/main/java/com/start9labs/start9companion/MainActivity.java
+++ android/app/src/main/java/com/start9labs/start9companion/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends BridgeActivity {
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
-       Ex: add(TorClientPlugin.class);
+       add(TorClientPlugin.class);
     }});
   }
 }
